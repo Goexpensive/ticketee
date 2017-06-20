9cc04fa5b0adc12612dc4078c06f04b422761c6b
Fabio <goexpensive@gmail.com>
2017-06-20 20:55:45 -0300

    Implement creating tickets for a project

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170615235738) do
+ActiveRecord::Schema.define(version: 20170620232838) do
 
   create_table "projects", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "name"
@@ -19,4 +19,14 @@ ActiveRecord::Schema.define(version: 20170615235738) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "tickets", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "name"
+    t.text "description"
+    t.bigint "project_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["project_id"], name: "index_tickets_on_project_id"
+  end
+
+  add_foreign_key "tickets", "projects"
 end
