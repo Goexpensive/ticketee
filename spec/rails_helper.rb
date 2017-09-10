1f55bed4bce8f6b12f139a0eac4d9399a5c8fcfa
Fabio <goexpensive@gmail.com>
2017-09-09 21:04:03 -0300

    Protect state_id from users who do not have permission to change it

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -61,4 +61,5 @@ RSpec.configure do |config|
   # config.filter_gems_from_backtrace("gem name")
   config.include Warden::Test::Helpers, type: :feature
   config.after(type: :feature) { Warden.test_reset! }
+  config.include Devise::Test::ControllerHelpers, type: :controller
 end
